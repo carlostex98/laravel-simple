3f0029a8b65c3085e3d2bbc0277581d43bb4a7a2
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-05-27 22:33:41 -0600

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,27 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{ HTML::script('js/uikit.min.js') }}
-    {{ HTML::script('js/uikit-icons.min.js') }}
-    {{ HTML::style('css/uikit.min.css') }}
+    <link rel="stylesheet" href="/css/uikit.min.css">
+    <script src="/js/uikit.min.js"></script>
+    <script src="/js/uikit-icons.min.js"></script>
+    <script></script>
     <title>Directorio</title>
 </head>
 
 <body>
-    <div class="uk-grid-small uk-child-width-auto uk-margin" uk-grid uk-countdown="date: 2020-06-03T03:24:07+00:00">
-        <div>
-            <div class="uk-countdown-number uk-countdown-days"></div>
-        </div>
-        <div>
-            <div class="uk-countdown-number uk-countdown-hours"></div>
-        </div>
-        <div>
-            <div class="uk-countdown-number uk-countdown-minutes"></div>
-        </div>
-        <div>
-            <div class="uk-countdown-number uk-countdown-seconds"></div>
-        </div>
+    <div class="uk-flex">
+        <div class="uk-card uk-card-default uk-card-body">Item 1</div>
+        <div class="uk-card uk-card-default uk-card-body">Item 2</div>
+        <div class="uk-card uk-card-default uk-card-body">Item 3</div>
+        <div class="uk-card uk-card-default uk-card-body">Item 4</div>
+        <div class="uk-card uk-card-default uk-card-body">Item 5</div>
     </div>
+
+
 </body>
 
 </html>
